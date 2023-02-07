dc439e2362a1968e2573d2a0834c7cb793dc99f9
sadik312 <sadikislam46@gmail.com>
2023-02-07 11:21:41 +0000

    new data seeded in seed1.sql file

---
+++ server/db/seed1.sql
@@ -0,0 +1,13 @@
+INSERT INTO thinkpads (name, model, price, description)
+VALUES 
+("ThinkPad X1 Carbon", "20QD000JUS", 1799.99, "14-inch business laptop with 10th Gen Intel Core processors"),
+("ThinkPad X1 Yoga", "20QB000NUS", 1899.99, "14-inch 2-in-1 laptop with 360-degree hinge and Wacom AES pen support"),
+("ThinkPad T14", "20S5A00NUS", 999.99, "14-inch laptop with AMD Ryzen processors and long battery life"),
+("ThinkPad T15p", "20T50010US", 1199.99, "15.6-inch laptop with powerful Intel Core processors and NVIDIA graphics"),
+("ThinkPad L14", "20U1S00A00", 799.99, "14-inch laptop with Intel Celeron processors and slim design"),
+("ThinkPad T400", "2767-CTO", 999.99, "14-inch business laptop with Intel Core 2 Duo processors and long battery life"),
+("ThinkPad T420", "4178-CTO", 1099.99, "14-inch laptop with Intel Core i5 processors and enhanced graphics"),
+("ThinkPad X220", "4299-CTO", 1299.99, "12.5-inch ultraportable laptop with Intel Core i7 processors and high-resolution display"),
+("ThinkPad X60", "1707-CTO", 999.99, "12.1-inch ultraportable laptop with Intel Core 2 Duo processors and long battery life"),
+("ThinkPad X61", "7674-CTO", 1099.99, "12.1-inch ultraportable laptop with Intel Core 2 Duo processors and enhanced graphics");
+
